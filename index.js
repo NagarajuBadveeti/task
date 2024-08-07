976815e2e5d845409a81b13d16343fb1d9bb3d56
NagarajuBadveeti <badveetiraju@gmail.com>
2024-08-07 16:23:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
- 
-//// Learn about Node.js modules and npm. Explore core modules (fs, path, http). Create a simple file server.
-const fs = require('fs');
-fs.readFile('notes.txt', 'utf8', (err, data) => {
-    if (err) throw err;//const fs = require('fs'); // Import fs module
-    //fs.writeFileSync('notes.txt', 'I hate to code');
-    //fs.writeFileSync('notes.txt', 'I  am good');
-    
-    console.log(data);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const logger_1 = __importDefault(require("./logger"));
+const app = (0, express_1.default)();
+const port = 9944;
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
 });
-
-const path = require('path');
-const filePath = '/home/user/docs/file.txt';
-console.log(path.dirname(filePath)); // Output: /home/user/docs
-
-console.log("working directory: ");
+// Route that triggers an error
+app.get('/error', (req, res, next) => {
+    const err = new Error('Something went wrong!');
+    next(err); // Passes the error to the error handling middleware
+});
+// Error handling middleware
+app.use((err, req, res, next) => {
+    logger_1.default.error(err.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+// Start the server
+app.listen(port, () => {
+    logger_1.default.info(`Server running at http://localhost:${port}/`);
+});
